bacd1f1d0159db6d7c49f1f17d6008118801fae9
uribo <ur1b0.dev@gmail.com>
2023-03-06 18:52:56 +0900

    処理時間計測関数を追加

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from PIL import Image
 import datetime
 import time
 import os
+import sys
 
 TRIALS = 20
 TIME_AJUST = 0.070
 SAVE_PATH = "./tmp"
 
-def saveImage(image: Image.Image,path: str, filename: str):
-    time_stamp = datetime.datetime.now()
-    image.save(f"{path}/{time_stamp.strftime('%Y%m%d%H%M%S%f')}_{filename}.png")
+def saveImage(image: Image.Image, filename: str):
+    image.save(f"{SAVE_PATH}/{filename}.png")
+
+def measure_process_time(process):
+    time_start = time.time()
+    process(TRIALS)
+    time_elpsed = time.time() - time_start
+    print(f"total:{format(time_elpsed,'.3f')}[s]\taverage:{format(time_elpsed/TRIALS,'.3f')}[s]")
+
 
 def experiment_img_gen(trial):
     for i in range(trial):
@@ -20,24 +27,21 @@ def experiment_img_gen(trial):
         time.sleep(TIME_AJUST)
 
 def experiment_simple(trial: int):
+    time_stamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
     for i in range(trial):
         im = Image.effect_noise((640,480), 512)
         # 画像生成と合わせて100msくらいに調整
         time.sleep(TIME_AJUST)
-        saveImage(im, SAVE_PATH, i)
+        saveImage(im, f"{time_stamp}_{i}")
 
 if not os.path.exists(SAVE_PATH):
     os.makedirs(SAVE_PATH)
 
 print(f"trials:{TRIALS}")
 
-time_start = time.time()
-experiment_img_gen(TRIALS)
-time_ex_img_gen = time.time() - time_start
-print(f"ex_img_gen:{format(time_ex_img_gen,'.3f')}\taverage:{format(time_ex_img_gen/TRIALS,'.3f')}")
+print(f"- execute:{experiment_img_gen.__name__}")
+measure_process_time(experiment_img_gen)
 
-time_start = time.time()
-experiment_simple(TRIALS)
-time_ex_simple = time.time() - time_start
-print(f"ex_simple:{format(time_ex_simple,'.3f')}\t\taverage:{format(time_ex_simple/TRIALS,'.3f')}")
+print(f"execute:{experiment_simple.__name__}")
+measure_process_time(experiment_simple)
 
